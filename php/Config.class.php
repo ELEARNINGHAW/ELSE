495305824b010062c4db5db8dda0de4f4149f07f
Werner <werner.welte@gmx.de>
2020-05-06 18:37:41 +0200

    Config.class to config.ini

---
+++ php/Config.class.php
@@ -0,0 +1,38 @@
+<?php
+class ConfigELSE
+{  
+var $C;
+var $CFG;
+
+function getConf()
+{
+  $conf = parse_ini_file("../php/config/config.ini", true);
+  foreach ( $conf[ 'SEM' ] as $sem => $startend ) { $cfg[ 'SEM' ][ $sem ] =  explode( ',',  str_replace( ' ' , '' , $startend ) ) ; }
+  $conf[ 'SEM' ] = $cfg[ 'SEM' ];
+
+  $pat = explode( '/', $_SERVER[ 'SCRIPT_FILENAME' ] );  ## Ermittelt in welchem Verzeichnis dieses Programm liegt (ELSE vs. ELSE-TEST)
+  array_pop($pat); array_pop($pat);
+  $conf[ 'CONF' ][ 'cwd' ] = array_pop($pat);
+
+  if (!isset ($conf['SERVER']))
+  {
+     $conf[ 'SERVER' ] =  $conf[ $conf[ 'CONF' ][ 'cwd' ] ];
+  }
+
+  return $conf;
+}
+
+
+
+function __construct( $CONSTANT )
+{
+  $this->C = $CONSTANT;
+  $this->CFG = $this->getConf();
+
+  $_SESSION[ 'CFG'    ] =  $this -> CFG;
+  $_SESSION[ 'CON'  ] =  $CONSTANT;
+#  deb($_SESSION);
+}
+
+}
+?>
